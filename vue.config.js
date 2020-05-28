75097f17ddc745f1d29cefecab97f5c26f944d98
吴文杰 <wuwj@kingnet.com>
2020-05-28 17:27:09 +0800

    add compression-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
+
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+
 module.exports = {
   // 选项
 	// pubilcPath:'./',
 	lintOnSave: false,
+	productionSourceMap: false,
 	devServer: {
 		port: 8079,
 		index: 'index.html',
@@ -26,5 +31,30 @@ module.exports = {
 	css: {
 		extract: false
 	},
-	
+
+    chainWebpack: config => {
+        config.optimization.minimize(true);
+	},
+	//Webpack配置
+	configureWebpack: config => {
+		const plugins = [];
+		//生产环境确认
+
+		//production版本console.log 打包时 自动注释
+		config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+
+		//压缩
+		plugins.push(
+			//gzip压缩
+			new CompressionWebpackPlugin({
+				filename: "[path].gz[query]",
+				algorithm: "gzip",
+				test: productionGzipExtensions,
+				threshold: 10240,
+				minRatio: 0.8
+			})
+		);
+		config.plugins = [...config.plugins, ...plugins];
+	},
+
 }
